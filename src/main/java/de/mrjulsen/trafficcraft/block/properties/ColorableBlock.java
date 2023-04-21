b03ef96bfc4c8f7bc9866331db7512aa74ada484
MisterJulsen <geisterdrache02@gmail.com>
2023-04-21 18:45:57 +0200

    Removed unused import

--- src/main/java/de/mrjulsen/trafficcraft/block/properties/ColorableBlock.java
+++ src/main/java/de/mrjulsen/trafficcraft/block/properties/ColorableBlock.java
@@ -12,7 +12,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
