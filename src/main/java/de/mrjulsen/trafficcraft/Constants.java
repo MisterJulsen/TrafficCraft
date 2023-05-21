f002bb81cfc434844cbaeb7fbe8dabd18e1d648d
MisterJulsen <geisterdrache02@gmail.com>
2023-05-21 18:18:55 +0200

    Added new traffic signs

--- src/main/java/de/mrjulsen/trafficcraft/Constants.java
+++ src/main/java/de/mrjulsen/trafficcraft/Constants.java
@@ -6,15 +6,15 @@ import de.mrjulsen.trafficcraft.block.properties.TrafficSignShape;
 
 public class Constants {
     public static final HashMap<TrafficSignShape, Integer> SIGN_PATTERNS = new HashMap<>() {{
-        put(TrafficSignShape.CIRCLE, 112);
+        put(TrafficSignShape.CIRCLE, 133);
         put(TrafficSignShape.SQUARE, 105);
         put(TrafficSignShape.TRIANGLE, 96);
         put(TrafficSignShape.DIAMOND, 115);
         put(TrafficSignShape.RECTANGLE, 84);
-        put(TrafficSignShape.MISC, 75);
+        put(TrafficSignShape.MISC, 78);
     }};
 
-    public static final int MAX_TRAFFIC_SIGN_STATES = 116;
+    public static final int MAX_TRAFFIC_SIGN_STATES = 134;
     public static final int MAX_ASPHALT_PATTERNS = 323;
     public static final int MAX_PAINT = 64;
     public static final int TICKS_PER_DAY = 24000;
