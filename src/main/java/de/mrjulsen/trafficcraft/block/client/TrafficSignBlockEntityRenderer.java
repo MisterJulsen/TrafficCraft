692deee2b490f95462c4d02a1f0a347492b36708
MisterJulsen <geisterdrache02@gmail.com>
2023-09-15 13:49:55 +0200

    Fixed: Back side of misc traffic sign not rendering

--- src/main/java/de/mrjulsen/trafficcraft/block/client/TrafficSignBlockEntityRenderer.java
+++ src/main/java/de/mrjulsen/trafficcraft/block/client/TrafficSignBlockEntityRenderer.java
@@ -70,7 +70,7 @@ public class TrafficSignBlockEntityRenderer implements BlockEntityRenderer<Traff
                 1, 1, 0,
                 0, 0,
                 1, 1,
-                1, 1, 1, 1,
+                255, 255, 255, 255,
                 lu, lv
             );
             pPoseStack.popPose();
