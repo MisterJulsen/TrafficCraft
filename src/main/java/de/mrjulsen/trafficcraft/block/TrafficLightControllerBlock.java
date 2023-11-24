9cd0a44987414c684f4c74e9209f637be66fe68c
MisterJulsen <geisterdrache02@gmail.com>
2023-11-24 15:02:55 +0100

    Feature: Traffic Light Controller ist overheated in the nether

--- src/main/java/de/mrjulsen/trafficcraft/block/TrafficLightControllerBlock.java
+++ src/main/java/de/mrjulsen/trafficcraft/block/TrafficLightControllerBlock.java
@@ -1,5 +1,7 @@
 package de.mrjulsen.trafficcraft.block;
 
+import java.util.Random;
+
 import javax.annotation.Nullable;
 
 import de.mrjulsen.trafficcraft.block.data.TrafficLightTrigger;
@@ -10,11 +12,16 @@ import de.mrjulsen.trafficcraft.item.WrenchItem;
 import de.mrjulsen.trafficcraft.registry.ModBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
@@ -67,6 +74,19 @@ public class TrafficLightControllerBlock extends BaseEntityBlock {
     protected void createBlockStateDefinition(Builder<Block, BlockState> pBuilder) {
         super.createBlockStateDefinition(pBuilder);
         pBuilder.add(FACING);
+    }
+
+    public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
+        if (pLevel.dimensionType().ultraWarm() && !pEntity.fireImmune() && pEntity instanceof LivingEntity && !EnchantmentHelper.hasFrostWalker((LivingEntity)pEntity)) {
+            pEntity.hurt(DamageSource.HOT_FLOOR, 1.0F);
+        }
+
+        super.stepOn(pLevel, pPos, pState, pEntity);
+    }
+
+    @Override
+    public void animateTick(BlockState pState, Level pLevel, BlockPos pPos, Random pRandom) {
+        pLevel.addParticle(ParticleTypes.SMOKE, pPos.getX() + 0.5D, pPos.getY() + 0.5D, pPos.getZ() + 0.5D, pRandom.nextDouble(-0.05D, 0.05D), pRandom.nextDouble(-0.05D, 0.05D), pRandom.nextDouble(-0.05D, 0.05D));
     }
 
     @Override
