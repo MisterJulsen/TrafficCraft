1a42028dba96c8294360d8985eba0a4feed8ad84
MisterJulsen <geisterdrache02@gmail.com>
2023-12-04 23:49:44 +0100

    Fixed crash when rendering traffic sign

--- src/main/java/de/mrjulsen/trafficcraft/client/ber/TrafficSignBlockEntityRenderer.java
+++ src/main/java/de/mrjulsen/trafficcraft/client/ber/TrafficSignBlockEntityRenderer.java
@@ -5,6 +5,8 @@ import java.util.BitSet;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Vector3f;
+
+import de.mrjulsen.trafficcraft.ModMain;
 import de.mrjulsen.trafficcraft.block.TrafficSignBlock;
 import de.mrjulsen.trafficcraft.block.data.TrafficSignShape;
 import de.mrjulsen.trafficcraft.block.entity.TrafficSignBlockEntity;
@@ -22,8 +24,11 @@ import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.ChunkAccess;
 
 public class TrafficSignBlockEntityRenderer implements BlockEntityRenderer<TrafficSignBlockEntity> {
 
@@ -34,6 +39,10 @@ public class TrafficSignBlockEntityRenderer implements BlockEntityRenderer<Traff
     @SuppressWarnings("resource")
     public void render(TrafficSignBlockEntity pBlockEntity, float pPartialTick, PoseStack pPoseStack, MultiBufferSource pBufferSource, int pPackedLight, int pPackedOverlay) {
         BlockState blockstate = pBlockEntity.getBlockState();
+
+        if (pBlockEntity == null || blockstate == null) {
+            return;
+        }
 
         DynamicTexture tex = TrafficSignTextureCacheClient.getTexture(pBlockEntity, pBlockEntity.getTexture(), pBlockEntity.getBlockState().getValue(TrafficSignBlock.SHAPE) == TrafficSignShape.MISC, (texture) -> {
             pBlockEntity.setBase64Texture(TrafficSignTextureCacheClient.textureToBase64(pBlockEntity));
@@ -96,22 +105,32 @@ public class TrafficSignBlockEntityRenderer implements BlockEntityRenderer<Traff
     @SuppressWarnings("resource")
     public static void addQuadSide(BlockEntity be, BlockState state, Direction direction, VertexConsumer builder, PoseStack pPoseStack, float x0, float y0, float z0, float x1, float y1, float z1, float u0, float v0, float u1, float v1, float r, float g, float b, float a, int packedLight) {
 
-        if (Minecraft.getInstance().options.ambientOcclusion == AmbientOcclusionStatus.OFF) {
+        if (Minecraft.getInstance().options.ambientOcclusion == AmbientOcclusionStatus.OFF || be.getLevel() == null || be.getBlockPos() == null) {
             addVert(builder, pPoseStack, x0, y1, z0, u0, v0, r, g, b, a, packedLight & 0xFFFF, (packedLight >> 16) & 0xFFFF);
             addVert(builder, pPoseStack, x0, y0, z0, u0, v1, r, g, b, a, packedLight & 0xFFFF, (packedLight >> 16) & 0xFFFF);
             addVert(builder, pPoseStack, x1, y0, z1, u1, v1, r, g, b, a, packedLight & 0xFFFF, (packedLight >> 16) & 0xFFFF);
             addVert(builder, pPoseStack, x1, y1, z1, u1, v0, r, g, b, a, packedLight & 0xFFFF, (packedLight >> 16) & 0xFFFF);
         } else {
-            float[] afloat = new float[Direction.values().length * 2];
-            BitSet bitset = new BitSet(3);
-            ModelBlockRenderer.AmbientOcclusionFace ao = Minecraft.getInstance().getBlockRenderer().getModelRenderer().new AmbientOcclusionFace();
-            BlockPos origin = be.getLevel().getChunk(be.getBlockPos()).getPos().getWorldPosition();
-            ao.calculate(new RenderRegionCache().createRegion(be.getLevel(), origin.offset(-1, -1, -1), origin.offset(16, 16, 16), 1), state, be.getBlockPos(), direction, afloat, bitset, true);
-            
-            addVert(builder, pPoseStack, x0, y1, z0, u0, v0, r * ao.brightness[0], g * ao.brightness[0], b * ao.brightness[0], a, ao.lightmap[0] & 0xFFFF, (ao.lightmap[0] >> 16) & 0xFFFF);
-            addVert(builder, pPoseStack, x0, y0, z0, u0, v1, r * ao.brightness[1], g * ao.brightness[1], b * ao.brightness[1], a, ao.lightmap[1] & 0xFFFF, (ao.lightmap[1] >> 16) & 0xFFFF);
-            addVert(builder, pPoseStack, x1, y0, z1, u1, v1, r * ao.brightness[2], g * ao.brightness[2], b * ao.brightness[2], a, ao.lightmap[2] & 0xFFFF, (ao.lightmap[2] >> 16) & 0xFFFF);
-            addVert(builder, pPoseStack, x1, y1, z1, u1, v0, r * ao.brightness[3], g * ao.brightness[3], b * ao.brightness[3], a, ao.lightmap[3] & 0xFFFF, (ao.lightmap[3] >> 16) & 0xFFFF);
+            try {
+                float[] afloat = new float[Direction.values().length * 2];
+                BitSet bitset = new BitSet(3);
+                ModelBlockRenderer.AmbientOcclusionFace ao = Minecraft.getInstance().getBlockRenderer().getModelRenderer().new AmbientOcclusionFace();
+
+                BlockPos origin = be.getLevel().getChunk(be.getBlockPos()).getPos().getWorldPosition();
+                BlockAndTintGetter batg = new RenderRegionCache().createRegion(be.getLevel(), origin.offset(-1, -1, -1), origin.offset(16, 16, 16), 1);
+                if (batg == null) {
+                    ModMain.LOGGER.warn("Chunk Region Renderer was null.");
+                    return;
+                }
+                ao.calculate(batg, state, be.getBlockPos(), direction, afloat, bitset, true);
+                
+                addVert(builder, pPoseStack, x0, y1, z0, u0, v0, r * ao.brightness[0], g * ao.brightness[0], b * ao.brightness[0], a, ao.lightmap[0] & 0xFFFF, (ao.lightmap[0] >> 16) & 0xFFFF);
+                addVert(builder, pPoseStack, x0, y0, z0, u0, v1, r * ao.brightness[1], g * ao.brightness[1], b * ao.brightness[1], a, ao.lightmap[1] & 0xFFFF, (ao.lightmap[1] >> 16) & 0xFFFF);
+                addVert(builder, pPoseStack, x1, y0, z1, u1, v1, r * ao.brightness[2], g * ao.brightness[2], b * ao.brightness[2], a, ao.lightmap[2] & 0xFFFF, (ao.lightmap[2] >> 16) & 0xFFFF);
+                addVert(builder, pPoseStack, x1, y1, z1, u1, v0, r * ao.brightness[3], g * ao.brightness[3], b * ao.brightness[3], a, ao.lightmap[3] & 0xFFFF, (ao.lightmap[3] >> 16) & 0xFFFF);
+            } catch (Exception e) {
+                ModMain.LOGGER.error("Error while rendering Traffic Sign with AO.", e);
+            }
         }
 
     }
