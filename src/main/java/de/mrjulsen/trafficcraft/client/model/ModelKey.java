253687fb6e72b243c5d4c2d7f82c9d4740c5bc7d
MisterJulsen <julian.ramsperger@t-online.de>
2023-12-12 19:16:10 +0100

    Removed unused classes

--- src/main/java/de/mrjulsen/trafficcraft/client/model/ModelKey.java
+++
@@ -1,7 +0,0 @@
-package de.mrjulsen.trafficcraft.client.model;
-
-import javax.annotation.Nullable;
-
-import net.minecraft.client.resources.model.ModelState;
-
-public record ModelKey(boolean generating, boolean collecting, boolean actuallyGenerating, @Nullable ModelState modelState) { }
