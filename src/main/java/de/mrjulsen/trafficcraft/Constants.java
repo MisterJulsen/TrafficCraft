113633000aed8091d7b5c7e6ca271bd2b16f506a
MisterJulsen <geisterdrache02@gmail.com>
2023-12-26 23:11:27 +0100

    Added new Constants

--- src/main/java/de/mrjulsen/trafficcraft/Constants.java
+++ src/main/java/de/mrjulsen/trafficcraft/Constants.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import de.mrjulsen.mcdragonlib.client.gui.GuiUtils;
 import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 
 public class Constants {
@@ -14,6 +15,16 @@ public class Constants {
     public static final int TRAFFIC_CONE_BASE_COLOR = 0xFFD12725;
 
     public static final MutableComponent CREATIVE_MODE_ONLY_TOOLTIP = GuiUtils.translate("core.trafficcraft.creative_only.tooltip").withStyle(ChatFormatting.GOLD);
+    
+    public static final Component textCopy = GuiUtils.translate("core.trafficcraft.common.copy");
+    public static final Component textPaste = GuiUtils.translate("core.trafficcraft.common.paste");
+
+    public static final String GERMAN_LOCAL_CODE = "de";
+
+    public static final String WIKIPEDIA_TRAFFIC_LIGHT_ID = "Q8004";
+    public static final String WIKIPEDIA_GERMAN_TRAM_SIGNAL_ID = "Q2354774";
 
     public static final Random RANDOM = new Random();
 }
+
+
