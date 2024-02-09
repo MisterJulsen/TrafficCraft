2b91df3567c679f614a0a37843a1adee7e704725
MisterJulsen <geisterdrache02@gmail.com>
2024-02-09 08:38:50 +0100

    Fixed rare crash

--- src/main/java/de/mrjulsen/trafficcraft/client/screen/TrafficLightScheduleEditor.java
+++ src/main/java/de/mrjulsen/trafficcraft/client/screen/TrafficLightScheduleEditor.java
@@ -109,7 +109,7 @@ public class TrafficLightScheduleEditor extends CommonScreen {
                         TrafficLightType savedType = phaseIdTypes.get(phaseId);
                         if (savedType != null && savedType != type) {
                             phaseIdTypes.remove(phaseId);
-                            phaseIdTypes.put(phaseId, null);
+                            phaseIdTypes.put(phaseId, type);
                         }
                     } else {
                         phaseIdTypes.put(phaseId, type);
@@ -278,7 +278,14 @@ public class TrafficLightScheduleEditor extends CommonScreen {
     }
 
     private void move(TrafficLightScheduleEntryData entry, int offset) {
-        int index = schedule.getEntries().indexOf(entry);
+        int index = -1;
+        List<TrafficLightScheduleEntryData> entries = schedule.getEntries();
+        for (int i = 0; i < entries.size(); i++) {
+            if (entries.get(i) == entry) {
+                index = i;
+                break;
+            }
+        }
         int newIndex = index + offset;
 
         if (newIndex < 0 || newIndex >= schedule.getEntries().size()) {
