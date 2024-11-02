7d2e5c229b38c5c46f34dd3968a0aa3ce634f7b7
MrJulsen <geisterdrache02@gmail.com>
2024-11-02 23:51:25 +0100

    Fixed issues on server

--- common/src/main/java/de/mrjulsen/trafficcraft/recipe/DamageableItemRecipe.java
+++ common/src/main/java/de/mrjulsen/trafficcraft/recipe/DamageableItemRecipe.java
@@ -1,5 +1,6 @@
 package de.mrjulsen.trafficcraft.recipe;
 
+import de.mrjulsen.trafficcraft.registry.ModItems;
 import net.minecraft.core.NonNullList;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.Item;
@@ -17,7 +18,7 @@ public class DamageableItemRecipe extends ShapelessRecipe {
 
     @Override
     public RecipeSerializer<?> getSerializer() {
-        return DamageableItemRecipeSerializer.INSTANCE;
+        return ModItems.DAMAGEABLE_ITEM_RECIPE.get();
     }
     
     @Override
