2ac488f32a235f125f9e24c776439b85d20b686f
MisterJulsen <julian.ramsperger@t-online.de>
2024-11-07 09:28:35 +0100

    Fixed crash with custom tooltip

--- forge/src/main/java/de/mrjulsen/trafficcraft/forge/CrossPlatformImpl.java
+++ forge/src/main/java/de/mrjulsen/trafficcraft/forge/CrossPlatformImpl.java
@@ -1,10 +1,17 @@
 package de.mrjulsen.trafficcraft.forge;
 
+import java.util.function.Function;
+
 import de.mrjulsen.trafficcraft.TrafficCraft;
 import de.mrjulsen.trafficcraft.config.ModCommonConfig;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 
@@ -16,5 +23,10 @@ public final class CrossPlatformImpl {
 
     public static void registerConfig() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ModCommonConfig.SPEC, TrafficCraft.MOD_ID + "-common.toml");
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static <T extends TooltipComponent> void registerTooltipComponentFactory(Class<T> cls, Function<? super T, ? extends ClientTooltipComponent> factory) {
+        MinecraftForgeClient.registerTooltipComponentFactory(cls, factory);
     }
 }
